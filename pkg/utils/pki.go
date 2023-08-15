2dba4720604591221e30a68f7e63ea846b3f1b71
Matt Turner <git@mt165.co.uk>
2023-08-15 18:14:05 +0100

    Pass logging context to cert generation

--- pkg/utils/pki.go
+++ pkg/utils/pki.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"bytes"
+	"context"
 	"crypto"
 	"crypto/ecdsa"
 	"crypto/ed25519"
@@ -18,6 +19,8 @@ import (
 	"sync"
 	"time"
 
+	"github.com/tetratelabs/telemetry"
+
 	"github.com/mt-inside/http-log/internal/build"
 )
 
@@ -30,14 +33,16 @@ func init() {
 	certCache = make(map[string]*tls.Certificate)
 }
 
-func genCertPair(settings *x509.Certificate, parent *tls.Certificate, algo string) (*tls.Certificate, error) {
+func genCertPair(ctx context.Context, settings *x509.Certificate, parent *tls.Certificate, algo string) (*tls.Certificate, error) {
+
+	log := log.With(telemetry.KeyValuesFromContext(ctx)...)
 
 	if len(settings.DNSNames) > 1 {
 		panic(errors.New("only support one SAN atm"))
 	}
 
 	name := settings.DNSNames[0]
-	log := log.With("name", name)
+	log = log.With("name", name)
 
 	certCacheLock.Lock()
 	if cert, ok := certCache[name]; ok {
@@ -62,6 +67,8 @@ func genCertPair(settings *x509.Certificate, parent *tls.Certificate, algo strin
 
 	switch algo {
 	case "rsa":
+		log.Debug("Generating keypair", "algo", "rsa")
+
 		key, err := rsa.GenerateKey(rand.Reader, 4096)
 		if err != nil {
 			return nil, err
@@ -87,6 +94,8 @@ func genCertPair(settings *x509.Certificate, parent *tls.Certificate, algo strin
 		}
 
 	case "ecdsa":
+		log.Debug("Generating keypair", "algo", "ecdsa")
+
 		key, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 		if err != nil {
 			return nil, err
@@ -113,6 +122,8 @@ func genCertPair(settings *x509.Certificate, parent *tls.Certificate, algo strin
 		}
 
 	case "ed25519":
+		log.Debug("Generating keypair", "algo", "ed25519")
+
 		pubKey, key, err := ed25519.GenerateKey(rand.Reader)
 		if err != nil {
 			return nil, err
@@ -167,9 +178,10 @@ func genCertPair(settings *x509.Certificate, parent *tls.Certificate, algo strin
 	return &pair, err
 }
 
-func GenSelfSignedCa(algo string) (*tls.Certificate, error) {
+func GenSelfSignedCa(ctx context.Context, algo string) (*tls.Certificate, error) {
 
-	log.Debug("Generating ca cert")
+	log := log.With(telemetry.KeyValuesFromContext(ctx)...)
+	log.Info("Generating CA cert")
 
 	caSettings := &x509.Certificate{
 		Subject: pkix.Name{
@@ -184,12 +196,13 @@ func GenSelfSignedCa(algo string) (*tls.Certificate, error) {
 		BasicConstraintsValid: true,
 	}
 
-	return genCertPair(caSettings, nil, algo)
+	return genCertPair(ctx, caSettings, nil, algo)
 }
 
-func GenServingCert(helloInfo *tls.ClientHelloInfo, parent *tls.Certificate, algo string) (*tls.Certificate, error) {
+func GenServingCert(ctx context.Context, helloInfo *tls.ClientHelloInfo, parent *tls.Certificate, algo string) (*tls.Certificate, error) {
 
-	log.Debug("Generating serving cert")
+	log := log.With(telemetry.KeyValuesFromContext(ctx)...)
+	log.Info("Generating self-signed serving cert")
 
 	dnsName := "localhost"
 	if helloInfo.ServerName != "" {
@@ -211,5 +224,5 @@ func GenServingCert(helloInfo *tls.ClientHelloInfo, parent *tls.Certificate, alg
 		KeyUsage:     x509.KeyUsageDigitalSignature,
 	}
 
-	return genCertPair(servingSettings, parent, algo)
+	return genCertPair(ctx, servingSettings, parent, algo)
 }
